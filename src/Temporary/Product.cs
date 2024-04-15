4607ddabe17381402e947024d9b871f2e78ec766
Phil Bastian <phil.bastian@particular.net>
2024-04-15 13:06:27 +0800

    implement halfstar display

--- src/Temporary/Product.cs
+++ src/Temporary/Product.cs
@@ -18,13 +18,13 @@ public class Product : ICompositionRequestsHandler
         switch (productId)
         {
             case "09390028-3d57-4c34-977d-9eb78f146618":
-                vm.Product = CreateProduct(Guid.Parse(productId), "Pizza", "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg/640px-Eq_it-na_pizza-margherita_sep2005_sml.jpg", 5, 5, 3.5, 7, "food");
+                vm.Product = CreateProduct(Guid.Parse(productId), "Pizza", "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg/640px-Eq_it-na_pizza-margherita_sep2005_sml.jpg", 5, 5, 3.7, 7, "food");
                 break;
             case "ff899e9d-4033-48d4-b189-e6ef4a3dc25b":
                 vm.Product = CreateProduct(Guid.Parse(productId), "Fremont Stout", "https://www.surprisefactory.nl/media/48245/zelf-bier-brouwen.jpg?crop=0.0000%2C0.0572%2C0%2C0&cropmode=percentage&width=600&height=400&quality=80&token=g3bmX6OhH3", 10, 83, 4.5, 42, "beer");
                 break;
             case "0b3dcc85-110b-4491-9946-d20c0a51917b":
-                vm.Product = CreateProduct(Guid.Parse(productId), "Heineken", "https://media.danmurphys.com.au/dmo/product/804969-1.png?impolicy=PROD_LG", 0.5m, 1843, 4.5, 212, "beer");
+                vm.Product = CreateProduct(Guid.Parse(productId), "Heineken", "https://media.danmurphys.com.au/dmo/product/804969-1.png?impolicy=PROD_LG", 0.5m, 1843, 4.8, 212, "beer");
                 break;
         }
         //vm.Product = CreateProduct(Guid.Parse("ff899e9d-4033-48d4-b189-e6ef4a3dc25b"), "Fremont Stout", "https://www.surprisefactory.nl/media/48245/zelf-bier-brouwen.jpg?crop=0.0000%2C0.0572%2C0%2C0&cropmode=percentage&width=600&height=400&quality=80&token=g3bmX6OhH3", 10, 4.5, 42, "beer");
--- src/reactapp1.client/src/Product/ProductRating.jsx
+++ src/reactapp1.client/src/Product/ProductRating.jsx
@@ -1,4 +1,5 @@
 import fullStar from "@/assets/fullstar.png";
+import halfStar from "@/assets/halfstar.png";
 import emptyStar from "@/assets/emptystar.png";
 import { getProductRating, useGetProduceDetails } from "../modelService";
 
@@ -8,14 +9,16 @@ export default function ProductRating({ id, className, long = false }) {
   const { data: ratingData } = useGetProduceDetails(getProductRating, id);
   const starItems =
     ratingData &&
-    Array.from({ length: 5 }).map((_, i) =>
-      i + 1 < ratingData.stars ? fullStar : emptyStar
-    );
+    Array.from({ length: 5 }).map((_, i) => {
+      if (i + 1 < ratingData.stars + 0.3) return fullStar;
+      if (i + 1 < ratingData.stars + 0.8) return halfStar;
+      return emptyStar;
+    });
 
   return (
     ratingData && (
       <div className={`${styles.rating} ${className ?? ""}`}>
-        <div className={styles.stars}>
+        <div className={styles.stars} title={`${ratingData.stars} out of 5`}>
           {starItems.map((item, i) => (
             <img className={styles.star} key={i} src={item} alt="star" />
           ))}
