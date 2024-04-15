c5aacc6455eda67e01fe724b243b832ba444c5d3
Phil Bastian <phil.bastian@particular.net>
2024-04-15 09:33:34 +0800

    enable CORS for localhost UI

--- src/CompositionGateway/Program.cs
+++ src/CompositionGateway/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Options;
 using ServiceComposer.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.WithOrigins("https://localhost:5173")
+                   .AllowAnyHeader();
+        }));
 builder.Services.AddViewModelComposition();
 
 var app = builder.Build();
@@ -16,6 +24,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors();
 
 app.UseHttpsRedirection();
 app.MapCompositionHandlers();
