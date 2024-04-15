16075a357882181b9c6d94416ed00058e4afe5ba
Dennis <dvdstelt@gmail.com>
2024-04-15 20:57:20 +0200

    Added initial version to submit cart

---
+++ src/Temporary/CartSubmit.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Newtonsoft.Json.Linq;
+using ServiceComposer.AspNetCore;
+
+namespace OmNomNom.Website.ViewModelComposition;
+
+public class CartSubmit : ICompositionRequestsHandler
+{
+    [HttpPost("/cart/{orderId}")]
+    public async Task Handle(HttpRequest request)
+    {
+        var routeData = request.HttpContext.GetRouteData();
+        var orderId = Guid.Parse(routeData.Values["orderId"] as string ?? throw new InvalidOperationException("OrderId can't be empty"));
+
+        request.Body.Position = 0;
+        using var reader = new StreamReader(request.Body, Encoding.UTF8, leaveOpen: true );
+        var body = await reader.ReadToEndAsync();
+        var content = JObject.Parse(body);
+
+        //use the content object instance as needed
+    }
+}
