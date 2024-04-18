0f51a53241d8f0ecd9b8dcf57daefd85e05ae20a
Dennis <dvdstelt@gmail.com>
2024-04-18 13:50:39 +0200

    Initial project structure

---
+++ src/Catalog.ServiceComposition/Products/ProductsHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Dynamic;
+using Microsoft.AspNetCore.Http;
+using ServiceComposer.AspNetCore;
+
+namespace Catalog.ServiceComposition.Products;
+
+public class ProductsHandler : ICompositionRequestsHandler
+{
+    public Task Handle(HttpRequest request)
+    {
+        var vm = request.GetComposedResponseModel();
+
+
+    }
+
+    IDictionary<int, dynamic> MapToDictionary(IEnumerable<Guid> availableProducts)
+    {
+        var availableProductsModel = new Dictionary<Guid, dynamic>();
+
+        foreach (var id in availableProducts)
+        {
+            dynamic vm = new ExpandoObject();
+            vm.ProductId = productId;
+
+            availableProductsModel[id] = vm;
+        }
+
+        return availableProductsModel;
+    }
+}
