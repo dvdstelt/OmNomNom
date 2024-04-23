7bdbf81d59363ba5f6ce9fc76cd0c5ab2d54be20
Dennis van der Stelt <dvdstelt@gmail.com>
2024-04-23 23:05:38 +0200

    Added delay of 1 second to shipping the order

--- src/Shipping.Endpoint/Handlers/ShipOrderRequestHandler.cs
+++ src/Shipping.Endpoint/Handlers/ShipOrderRequestHandler.cs
@@ -7,6 +7,8 @@ public class ShipOrderRequestHandler : IHandleMessages<ShipOrderRequest>
 {
     public async Task Handle(ShipOrderRequest message, IMessageHandlerContext context)
     {
+        await Task.Delay(1000, context.CancellationToken);
+        
         var reply = new ShipOrderReply();
         await context.Reply(reply);
     }
