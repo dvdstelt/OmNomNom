c761b69cc79d17e903abec53c7ef150eb7ec036a
Dennis van der Stelt <dvdstelt@gmail.com>
2024-04-23 22:34:38 +0200

    Added payment processing and succeeding

--- src/PaymentInfo.Endpoint/Handlers/CreditCardSubmitHandler.cs
+++ src/PaymentInfo.Endpoint/Handlers/CreditCardSubmitHandler.cs
@@ -7,15 +7,13 @@ namespace PaymentInfo.Endpoint.Handlers;
 
 public class SubmitPaymentInfoHandler(PaymentInfoDbContext dbContext) : IHandleMessages<SubmitPaymentInfo>
 {
-    readonly PaymentInfoDbContext dbContext = dbContext;
-
     public async Task Handle(SubmitPaymentInfo message, IMessageHandlerContext context)
     {
         var creditCardCollection = dbContext.Database.GetCollection<CreditCard>();
         var orderCollection = dbContext.Database.GetCollection<Order>();
         var creditCard = creditCardCollection.Query()
             .Where(s => s.CreditCardId == message.CreditCardId && s.CustomerId == message.CustomerId).Single();
-
+        
         if (creditCard == null)
         {
             var alert = new FraudDetection()
---
+++ src/PaymentInfo.Endpoint/Handlers/OrderAcceptedHandler.cs
@@ -0,0 +1,30 @@
+﻿using Catalog.Endpoint.Messages.Commands;
+using NServiceBus.Logging;
+using PaymentInfo.Data;
+using PaymentInfo.Data.Models;
+using PaymentInfo.Endpoint.Messages.Events;
+
+namespace PaymentInfo.Endpoint.Handlers;
+
+public class OrderAcceptedHandler(PaymentInfoDbContext dbContext) : IHandleMessages<OrderAccepted>
+{
+    static ILog log = LogManager.GetLogger<OrderAcceptedHandler>();
+
+    public async Task Handle(OrderAccepted message, IMessageHandlerContext context)
+    {
+        var orderCollection = dbContext.Database.GetCollection<Order>();
+        var creditCardCollection = dbContext.Database.GetCollection<CreditCard>();
+        
+        var order = orderCollection.Query().Where(s => s.OrderId == message.OrderId).Single();
+        var creditCard = creditCardCollection.Query().Where(s => s.CreditCardId == order.CreditCardId).Single();
+
+        // Send a message to IT/Ops to talk to payment gateway and subtract money.
+        // For now we'll assume it all worked and we'll move on.
+        
+        var @event = new PaymentSucceeded()
+        {
+            OrderId = message.OrderId
+        };
+        await context.Publish(@event);
+    }
+}
