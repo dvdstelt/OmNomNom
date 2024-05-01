b8ede29bd6cb6ef3d96eddc599af6a4524b4d583
Dennis <dvdstelt@gmail.com>
2024-05-01 09:37:33 +0200

    Added products from cart to delivery api

---
+++ src/Catalog.ServiceComposition/Checkout/ShoppingCartHandler.cs
@@ -0,0 +1,37 @@
+﻿using Catalog.Data;
+using Catalog.Data.Models;
+using Catalog.ServiceComposition.Events;
+using Catalog.ServiceComposition.Products;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using ServiceComposer.AspNetCore;
+
+namespace Catalog.ServiceComposition.Checkout;
+
+public class ShoppingCartHandler( CatalogDbContext dbContext) : ICompositionRequestsHandler
+{
+    [HttpGet("/buy/shipping/{orderId}")]
+    public async Task Handle(HttpRequest request)
+    {
+        var orderIdString = (string)request.HttpContext.GetRouteData().Values["orderId"]!;
+        var orderId = Guid.Parse(orderIdString);
+
+        var orderCollection = dbContext.Database.GetCollection<Order>();
+        var order = orderCollection.Query().Where(s => s.OrderId == orderId).SingleOrDefault();
+
+        var productsCollection = dbContext.Database.GetCollection<Product>();
+        var products = productsCollection.Query().ToList();
+        var orderedProducts = ShoppingCart.Mapper.MapToDictionary(order, products);
+
+        var context = request.GetCompositionContext();
+        await context.RaiseEvent(new CartLoaded()
+        {
+            OrderedProducts = orderedProducts
+        });
+
+        var vm = request.GetComposedResponseModel();
+        vm.OrderId = orderId;
+        vm.CartItems = orderedProducts.Values.ToList();
+    }
+}
