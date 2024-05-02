75d2f04de7f2c968336bcfe2d004e7c301c79a97
Phil Bastian <phil.bastian@particular.net>
2024-05-02 15:27:22 +0800

    fix logic for setting existing selected location on page reload

--- src/reactapp1.client/src/misc/LocationSelect.jsx
+++ src/reactapp1.client/src/misc/LocationSelect.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ReactModal from "react-modal";
 import { useLoadData, useLocalStorage } from "../misc";
 import { getLocations } from "../siteService";
@@ -11,17 +11,16 @@ export default function LocationSelect() {
   const [locationSelectOpen, setLocationSelectOpen] = useState(false);
   const [selectedLocationId, setSelectedLocationId] =
     useLocalStorage("locationId");
-  const { data: locations } = useLoadData(getLocations, null, {
-    callback: locationsLoaded,
-  });
+  const { data: locations } = useLoadData(getLocations);
 
-  function locationsLoaded(loaded) {
+  useEffect(() => {
     if (
-      !selectedLocationId ||
-      !loaded.find((location) => location.id === selectedLocationId)
+      locations &&
+      (!selectedLocationId ||
+        !locations.find((location) => location.id === selectedLocationId))
     )
-      setSelectedLocationId(loaded[0].id);
-  }
+      setSelectedLocationId(locations[0].id);
+  }, [selectedLocationId, locations]);
 
   function onCloseLocationSelectModal() {
     setLocationSelectOpen(false);
@@ -37,48 +36,51 @@ export default function LocationSelect() {
   );
 
   return (
-    <div>
-      <div
-        className={styles.locationSelect}
-        onClick={() => setLocationSelectOpen(true)}
-      >
-        <img className={styles.marker} src={marker} />
-        <div>
-          <span>Deliver to {selectedLocation?.name}</span>
-          <span
-            className={styles.locationName}
-          >{`${selectedLocation?.town}, ${selectedLocation.zipCode}`}</span>
-        </div>
-      </div>
-      <ReactModal
-        isOpen={locationSelectOpen}
-        onRequestClose={onCloseLocationSelectModal}
-        className="modal"
-        overlayClassName="modalOverlay"
-      >
-        <div className={styles.modal}>
-          <div className={styles.modalHeader}>Choose your location</div>
-          <div className={styles.modalContent}>
-            <span className={styles.instructions}>
-              Ship options and delivery speeds may vary for different locations
-            </span>
-            <div className={styles.locations}>
-              {(locations ?? []).map((location) => (
-                <div
-                  key={location.id}
-                  className={`${styles.location} ${
-                    location.id === selectedLocationId ? styles.active : ""
-                  }`}
-                  onClick={() => selectLocation(location.id)}
-                >
-                  <span className={styles.locationName}>{location.name}</span>
-                  <span>{`${location.town}, ${location.zipCode}`}</span>
-                </div>
-              ))}
-            </div>
+    selectedLocation && (
+      <div>
+        <div
+          className={styles.locationSelect}
+          onClick={() => setLocationSelectOpen(true)}
+        >
+          <img className={styles.marker} src={marker} />
+          <div>
+            <span>Deliver to {selectedLocation?.name}</span>
+            <span
+              className={styles.locationName}
+            >{`${selectedLocation?.town}, ${selectedLocation?.zipCode}`}</span>
           </div>
         </div>
-      </ReactModal>
-    </div>
+        <ReactModal
+          isOpen={locationSelectOpen}
+          onRequestClose={onCloseLocationSelectModal}
+          className="modal"
+          overlayClassName="modalOverlay"
+        >
+          <div className={styles.modal}>
+            <div className={styles.modalHeader}>Choose your location</div>
+            <div className={styles.modalContent}>
+              <span className={styles.instructions}>
+                Ship options and delivery speeds may vary for different
+                locations
+              </span>
+              <div className={styles.locations}>
+                {(locations ?? []).map((location) => (
+                  <div
+                    key={location.id}
+                    className={`${styles.location} ${
+                      location.id === selectedLocationId ? styles.active : ""
+                    }`}
+                    onClick={() => selectLocation(location.id)}
+                  >
+                    <span className={styles.locationName}>{location.name}</span>
+                    <span>{`${location.town}, ${location.zipCode}`}</span>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </ReactModal>
+      </div>
+    )
   );
 }
