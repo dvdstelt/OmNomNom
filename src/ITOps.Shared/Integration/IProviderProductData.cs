3edd460d5322afd5c441a6b76100038512a44207
Dennis <dvdstelt@gmail.com>
2024-05-02 20:40:45 +0200

    Reflection for provider interfaces

---
+++ src/ITOps.Shared/Integration/IProviderProductData.cs
@@ -0,0 +1,33 @@
+﻿namespace ITOps.Shared.Integration;
+
+public interface IProvideData
+{
+}
+
+public interface IProvideOrderData : IProvideData
+{
+    Order GetOrderInfo(Guid orderId);
+}
+
+public class Order
+{
+    public Address BillingAddress { get; set; }
+
+    public decimal TotalPrice { get; set; }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }
+    }
+
+    public class Address
+    {
+        public string FullName { get; set; }
+        public string Street { get; set; }
+        public string ZipCode { get; set; }
+        public string Town { get; set; }
+        public string Country { get; set; }
+    }
+}
