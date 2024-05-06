dbce5e3ef0371fb6426358fd1c3bc85910c196be
Dennis <dvdstelt@gmail.com>
2024-05-06 11:13:19 +0200

    Added extension methods for dbContext and fixed vm.Price for summary

--- src/Catalog.ServiceComposition/Checkout/SummaryHandler.cs
+++ src/Catalog.ServiceComposition/Checkout/SummaryHandler.cs
@@ -3,6 +3,7 @@ using Catalog.Data;
 using Catalog.Data.Models;
 using Catalog.ServiceComposition.Events;
 using Catalog.ServiceComposition.Helpers;
+using ITOps.Shared;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
@@ -18,37 +19,28 @@ public class SummaryHandler(CatalogDbContext dbContext, CacheHelper cacheHelper)
         var orderIdString = (string)request.HttpContext.GetRouteData().Values["orderId"]!;
         var orderId = Guid.Parse(orderIdString);
 
-        var orderCollection = dbContext.Database.GetCollection<Order>();
-        var order = orderCollection.Query().Where(s => s.OrderId == orderId).SingleOrDefault();
+        var order = dbContext.Where<Order>(s => s.OrderId == orderId).SingleOrDefault();
 
         if (order == null)
         {
             order = await cacheHelper.GetOrder(orderId);
         }
-        
+
         var productsModel = MapToDictionary(order.Products);
 
-        var vm = request.GetComposedResponseModel();
-        vm.TotalPrice = 0m;
-        try
+        var context = request.GetCompositionContext();
+        await context.RaiseEvent(new SummaryLoaded()
         {
-            var context = request.GetCompositionContext();
-            await context.RaiseEvent(new SummaryLoaded()
-            {
-                OrderId = orderId,
-                Products = productsModel
-            });
-        }
-        catch
-        {
-            //TODO: Dennis, why is Items empty in the SummaryLoaded event handler?
-        }
+            OrderId = orderId,
+            Products = productsModel
+        });
 
+        var vm = request.GetComposedResponseModel();
         vm.OrderId = orderId;
         vm.Products = productsModel;
     }
 
-    IDictionary<Guid,dynamic> MapToDictionary(List<OrderItem> products)
+    IDictionary<Guid, dynamic> MapToDictionary(List<OrderItem> products)
     {
         var productsViewModel = new Dictionary<Guid, dynamic>();
 
---
+++ src/ITOps.Shared/LiteDb/LiteDbContextExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+
+namespace ITOps.Shared;
+
+public static class LiteDbContextExtensions
+{
+    public static IEnumerable<T> GetAll<T>(this ILiteDbContext dbContext)
+    {
+        var collection = dbContext.Database.GetCollection<T>();
+        return collection.Query().ToEnumerable();
+    }
+
+    public static IEnumerable<T> Where<T>(this ILiteDbContext dbContext, Expression<Func<T, bool>> predicate)
+    {
+        var collection = dbContext.Database.GetCollection<T>();
+        return collection.Query().Where(predicate).ToEnumerable();
+    }
+}
