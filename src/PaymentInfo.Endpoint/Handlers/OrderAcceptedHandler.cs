3774efef8ac4ac5ffb22fc82147de2426b4f325f
Dennis van der Stelt <dvdstelt@gmail.com>
2026-05-08 15:01:17 +0200

    🔥 Drop unused Order/CreditCard lookup from OrderAcceptedHandler

--- src/PaymentInfo.Endpoint/Handlers/OrderAcceptedHandler.cs
+++ src/PaymentInfo.Endpoint/Handlers/OrderAcceptedHandler.cs
@@ -1,23 +1,15 @@
 using Catalog.Endpoint.Messages.Events;
-using Microsoft.EntityFrameworkCore;
 using NServiceBus.Logging;
-using PaymentInfo.Data;
 using PaymentInfo.Endpoint.Messages.Events;
 
 namespace PaymentInfo.Endpoint.Handlers;
 
-public class OrderAcceptedHandler(PaymentInfoDbContext dbContext) : IHandleMessages<OrderAccepted>
+public class OrderAcceptedHandler : IHandleMessages<OrderAccepted>
 {
     static ILog log = LogManager.GetLogger<OrderAcceptedHandler>();
 
     public async Task Handle(OrderAccepted message, IMessageHandlerContext context)
     {
-        _ = await (
-            from o in dbContext.Orders
-            join c in dbContext.CreditCards on o.CreditCardId equals c.CreditCardId
-            where o.OrderId == message.OrderId
-            select new { o, c }).SingleAsync(context.CancellationToken);
-
         // Send a message to IT/Ops to talk to payment gateway and subtract money.
         // For now we'll assume it all worked and we'll move on.
 
