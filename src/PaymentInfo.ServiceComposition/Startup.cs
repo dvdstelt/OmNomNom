50db7cde9dd299f89dd88ad87bd3c33e6345db58
Dennis van der Stelt <dvdstelt@gmail.com>
2026-05-13 10:51:06 +0200

    ♻️ Let each service boundary register its own NSB routing
    CompositionGateway no longer knows which messages route to which
    endpoint. Routing is contributed by each ServiceComposition's
    Startup via IConfigureEndpointRouting, auto-discovered from loaded
    assemblies inside EndpointConfiguration.Configure().

--- src/PaymentInfo.ServiceComposition/Startup.cs
+++ src/PaymentInfo.ServiceComposition/Startup.cs
@@ -1,16 +1,24 @@
+using ITOps.Shared.EndpointConfiguration;
 using ITOps.Shared.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using NServiceBus;
 using PaymentInfo.Data;
+using PaymentInfo.Endpoint.Messages.Commands;
 using ServiceComposer.AspNetCore;
 
 namespace PaymentInfo.ServiceComposition;
 
-public class Startup : IViewModelCompositionOptionsCustomization
+public class Startup : IViewModelCompositionOptionsCustomization, IConfigureEndpointRouting
 {
     public void Customize(ViewModelCompositionOptions options)
     {
         options.Services.AddDbContext<PaymentInfoDbContext>(opts =>
             opts.UseSqlite(SqliteStorage.GetConnectionString("paymentinfo")));
+    }
+
+    public void ConfigureRouting(RoutingSettings<LearningTransport> routing)
+    {
+        routing.RouteToEndpoint(typeof(SubmitPaymentInfo).Assembly, "PaymentInfo");
     }
 }
--- src/Shipping.ServiceComposition/Startup.cs
+++ src/Shipping.ServiceComposition/Startup.cs
@@ -1,16 +1,24 @@
+using ITOps.Shared.EndpointConfiguration;
 using ITOps.Shared.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using NServiceBus;
 using ServiceComposer.AspNetCore;
 using Shipping.Data;
+using Shipping.Endpoint.Messages.Commands;
 
 namespace Shipping.ServiceComposition;
 
-public class Startup : IViewModelCompositionOptionsCustomization
+public class Startup : IViewModelCompositionOptionsCustomization, IConfigureEndpointRouting
 {
     public void Customize(ViewModelCompositionOptions options)
     {
         options.Services.AddDbContext<ShippingDbContext>(opts =>
             opts.UseSqlite(SqliteStorage.GetConnectionString("shipping")));
+    }
+
+    public void ConfigureRouting(RoutingSettings<LearningTransport> routing)
+    {
+        routing.RouteToEndpoint(typeof(SubmitDeliveryOption).Assembly, "Shipping");
     }
 }
